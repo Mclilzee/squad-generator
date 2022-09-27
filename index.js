5ff086bc898ec32d6815f4bfc9c7e91038621624
Mclilzee <mc.lilzee@hotmail.com>
2022-09-27 07:39:48 +0200

    Seperate catagories

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const text = document.querySelector("textarea");
-
 const generateButton = document.querySelector("button");
 
+const accepted = new Set();
+const maybe = new Set();
+
 generateButton.addEventListener("click", () => {
-  let filterText = text.value.split("\n");
-  filterText = filterText.filter((word) => {
-    word.includes(/.Accepted./gm);
-  })
-  console.log(filterText);
+  const list = text.value.split("\n");
+
+  let fillingList = accepted;
+
+  for (let i = 0; i < list.length; i++) {
+    if ((/.*Accepted.*/gm).test(list[i])) {
+      fillingList = accepted;
+      continue;
+    } else if ((/.*Tentative.*/gm).test(list[i])) {
+      fillingList = maybe;
+      continue;
+    } else if ((/.*Declined.*/gm).test(list[i])) {
+      fillingList = new Set();
+      continue;
+    }
+
+    fillingList.add(list[i]);
+  }
 })
+
