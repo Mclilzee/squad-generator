cc6a7cd1da4116312535563c9795cb99d1775ae0
Mclilzee <mc.lilzee@hotmail.com>
2022-09-27 17:39:22 +0200

    Add factionbase database

--- src/squadsData.js
+++ src/squadsData.js
@@ -1,6 +1,16 @@
 const clerics = new Set(["Claramenta", "✠ ጠ𝔦𝘚ፕ𝔦 / L𝔦ፕН𝔦 ✠", "Aeliah", "BetterDays", "ganjarule", "Talonwife", "Tamrood", "WhiteDove_", "Averelle", "Galaxy_Girl", "Lady_EverLuv", "Aenara", "Nurse/Rosie"]);
 const mystics = new Set(["Elonei", "Maryiam", "Natta", "Fuji", "Alivia", "Gly", "Siachi"]);
 const blademasters = new Set(["slewdem", "grei", "SmashWolf", "DashRiprock"]);
+const factionBase = new Set(["Yehoa", "PureWhite", "Aenara", "iskiplegsday", "WoodUToDeath", "Darcytheus", "Claramenta", "Elonei", "Fuji", "Natta", "✠ ጠ𝔦𝘚ፕ𝔦 / L𝔦ፕН𝔦 ✠",
+  "Aeliah", "Anori", "Arttagan/boogs", "BetterDays/TerribleNights", "DreamSeeker", "EvoPony", "Ezratheus", "ganjarule", "isabo", "LastSplash", "LollyPopKiss", "Nubz",
+  "Qeries", "RapaNui", "RedoX", "slewdem", "StormKitty/CoolKitty", "Takoe", "Talonwife", "Tamrood", "xMilf", "AdversityK", "Aemirah", "Alitah/Shivy", "Alivia", "AlphaWarrior",
+  "AngioTensln", "ArrowRev", "Averelle", "buffwr", "BulleT", "Clubber", "Corbin", "Cyphur", "Dashriprock", "DisInfection", "Envyful", "estan/luiz", "FishyFret/Chayy", "Foxxeh", "Fyara", "Galaxy_Girl",
+  "GILF", "Gly", "grei", "Icepaw/Exo", "iHearts", "iiFake", "IlaSeeker", "Julianmede", "Kariami", "KayZier", "kenichi", "Kimilina/HowlingPuppy", "KitsukiFire", "krypto",
+  "Lady_EverLuv", "Lifesaber", "LimitedEdition", "Loganjl", "LunaRoja", "lynshei", "Maryiam", "Meisaro", "Methyd", "migothemanz", "MORT/Ingrid", "nigelus", "Nurse/Rosie",
+  "Parley/DAVID", "Pepprina", "PIERCE", "PullMyTailUwU", "Ranker", "Rausky", "RogueMona", "Roryy", "SANDYSXGAL", "Shuddemel", "Siachi", "SkyT", "SmashWolf", "SomiJinJin",
+  "StarAlfur", "tiago/picpay", "Toerleik", "UwUToDeath", "warlock", "WhiteDove_", "x_Adi_x", "Xaoziq", "xSlighterx/Agl", "Yinshen", "YSoooSeriious", "Zacromenstro", "zoey<3"
+
+])
 
 const concreteSquads = [
   ["WoodUToDeath", "GILF", "Aenara"],
